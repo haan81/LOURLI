db3b5888962bcf60850cc24f6017cd65ce86e128
haan M. LEE <lee.hanmin81@gmail.com>
2017-12-23 00:26:52 +0900

    - Save and Get a Action list from Array(ActList.js) - Enroll ActList.js file by bundle

---
+++ ActList.js
@@ -0,0 +1,14 @@
+var ActList = [
+    {Name:"Walking"},
+    {Name:"Weight Training"},
+    {Name:"App Dev"},
+    {Name:"Computer Science"},
+    {Name:"Foreign Language"},
+    {Name:"Development"},
+    {Name:"User Support"},
+    {Name:"READ a Book"}, 
+    {Name:"Sleep"},
+    {Name:"Rest"}
+];
+
+module.exports = ActList;
--- Pages/ActionListPage.js
+++ Pages/ActionListPage.js
@@ -1,12 +1,13 @@
-    var Observable = require('FuseJS/Observable');
-    var ETC_TEXT = Observable();
-
-    let items_workout=[{Name:"Walking"},{Name:"Weight Trainning"}]
-    let items_study=[{Name:"App Dev"},{Name:"Computer Science"},{Name:"Foreign Language"}]
-    let items_Work=[{Name:"Development"},{Name:"Support"}]
-    let items_Rest=[{Name:"Read a Book"},{Name:"Sleep"},{Name:"Rest"}]
-    let items_Etc=[{Name:"ENTER"}]
-    let items_Commute=[{Name:"Go to work"}, {Name:"Back home"}]
+    //var Observable = require('FuseJS/Observable');
+    //var ETC_TEXT = Observable();
+    var ActList = require("ActList");
+    
+    //let items_workout=[{Name:"Walking"},{Name:"Weight Trainning"}]
+    //let items_study=[{Name:"App Dev"},{Name:"Computer Science"},{Name:"Foreign Language"}]
+    //let items_Work=[{Name:"Development"},{Name:"Support"}]
+    //let items_Rest=[{Name:"Read a Book"},{Name:"Sleep"},{Name:"Rest"}]
+    //let items_Etc=[{Name:"ENTER"}]
+    //let items_Commute=[{Name:"Go to work"}, {Name:"Back home"}]
 
     function onClick(args) {
         var currentdate = new Date();
@@ -34,23 +35,24 @@
         }            
         else
         {
-            fetch('https://script.google.com/macros/s/AKfycbwmsx6T76fseLVwaA12wscU3iTed_XSQQ9doyNus39Uegl0kko/exec', {
-                method: 'post',
-                headers: { "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"},
-                body: 'Date=' + currDate + '&' +
-                      'Time=' + currTime + '&' +
-                      'Action=' + ETC_TEXT.value
-                }).then(function(response) {})
+            //fetch('https://script.google.com/macros/s/AKfycbwmsx6T76fseLVwaA12wscU3iTed_XSQQ9doyNus39Uegl0kko/exec', {
+            //    method: 'post',
+            //    headers: { "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"},
+            //    body: 'Date=' + currDate + '&' +
+            //          'Time=' + currTime + '&' +
+            //          'Action=' + ETC_TEXT.value
+            //    }).then(function(response) {})
         }           
     }       
 
     module.exports = {
-        items_workout: items_workout,
-        items_study: items_study,
-        items_Work: items_Work,
-        items_Rest: items_Rest,
-        items_Etc: items_Etc,
-        ETC_TEXT: ETC_TEXT,
-        items_Commute: items_Commute,
+        //items_workout: items_workout,
+        //items_study: items_study,
+        //items_Work: items_Work,
+        //items_Rest: items_Rest,
+        //items_Commute: items_Commute,
+        //items_Etc: items_Etc,
+        ActList: ActList,
+        //ETC_TEXT: ETC_TEXT,
         onClick: onClick
     }
